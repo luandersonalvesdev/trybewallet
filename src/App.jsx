9a81c842afc523df34516de535a0a24bca70bf0d
Luanderson Alves <luaoderson@gmail.com>
2023-04-11 15:29:32 -0300

    REQ01: form are working

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </div>
+    );
+  }
+}
---
+++ src/components/Form.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+const SIX = 6;
+
+export default class Form extends Component {
+  state = {
+    emailInput: '',
+    passwordInput: '',
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState(() => ({
+      [name]: value,
+    }));
+  };
+
+  render() {
+    const { emailInput, passwordInput } = this.state;
+    const checkEmailAndPassword = emailInput.includes('@')
+      && emailInput.includes('.com')
+      && passwordInput.length >= SIX;
+    return (
+      <form onSubmit={ (e) => e.preventDefault() }>
+        <label>
+          Email
+          <input
+            onChange={ this.handleChange }
+            name="emailInput"
+            type="text"
+            data-testid="email-input"
+          />
+        </label>
+        <label>
+          Senha
+          <input
+            onChange={ this.handleChange }
+            name="passwordInput"
+            type="password"
+            data-testid="password-input"
+          />
+        </label>
+        <button disabled={ !checkEmailAndPassword }>Entrar</button>
+      </form>
+    );
+  }
+}
